a81a15d41020bf2709346e42b086c86098f60b5c
vishal2082 <35956311+vishal2082@users.noreply.github.com>
2019-04-15 00:29:23 +0530

    Update HomeTest.java

--- src/test/java/com/code/HomeTest.java
+++ src/test/java/com/code/HomeTest.java
@@ -1,14 +1,10 @@
 package com.code;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
 public class HomeTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tests1 is executed ...");
 
+	}
 }
