d52f15e48369d6c96801aaa1e0d776499ee45fb3
VISHAL <vishal.kumar2@dxc.com>
2019-04-14 19:51:45 +0530

    first commit

---
+++ src/main/java/com/code/Home.java
@@ -0,0 +1,11 @@
+package com.code;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to home..");
+
+	}
+
+}
---
+++ src/test/java/com/code/HomeTest.java
@@ -0,0 +1,14 @@
+package com.code;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HomeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
